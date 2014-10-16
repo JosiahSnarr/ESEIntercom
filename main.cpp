2cf616fd0752cf38f26337cadd4eb6ef1866ec6b
Natesh Narain <nnaraindev@gmail.com>
2014-10-16 08:41:06 -0400

    implemented qt audio libraries and removed windows audio playbackcode(along with its dependencies)

--- main.cpp
+++ main.cpp
@@ -2,7 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "playback.h"
 #include "messagequeue.h"
 
 #include "mainwindow.h"
@@ -13,12 +12,12 @@
 #include <QDebug>
 
 // global vars from config file
-WORD  g_nChannels;
-DWORD g_nSamplesPerSec;
-WORD  g_nBitsPerSample;
-WORD  g_cbSize;
-long  g_lTimeout;
-short g_wAudioBufferSize;
+//WORD  g_nChannels;
+//DWORD g_nSamplesPerSec;
+//WORD  g_nBitsPerSample;
+//WORD  g_cbSize;
+//long  g_lTimeout;
+//short g_wAudioBufferSize;
 
 int main(int argc, char *argv[])
 {
